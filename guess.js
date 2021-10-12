db6db2145da3222a0ecd01a1c9756ffa24c2e36b
Don Vest <tsukasasan@hotmail.com>
2021-10-12 11:11:57 -0500

    Range Num section complete.

--- guess.js
+++ guess.js
@@ -20,3 +20,21 @@ let guess;
 // For storing user's response to play again or not.
 let playAgain;
 
+// 
+alert(`Welcome to "Guess That Number!" Please click "OK" to start the game.`);
+
+// Game restarts as long as restartGame has value of true.
+while (restartGame){
+    // Asks user to enter a number to set the upper bound for the random number that will be created (The number to be guessed).
+    rangeNum = prompt(`Please enter a maximum number for the range:`);
+
+    // Using parseInt to attempt to convert the user's response into a number value. NOTE: The value returned from a prompt is a string value. Also, if the value cannot be converted the value returned will be NaN (Not a Number).
+    rangeNum = parseInt(rangeNum); 
+
+    // Verifies the user's entry for a range number is a number greater than zero. NOTE: NaN has a default boolean value of false. Also, all numbers positive and negative, have a default boolean value of true except for zero which has a default boolean value of false.
+    while (!rangeNum || rangeNum < 1){
+        rangeNum = prompt(enterNumText);
+        rangeNum = parseInt(rangeNum);         
+    }
+    break;
+} 
